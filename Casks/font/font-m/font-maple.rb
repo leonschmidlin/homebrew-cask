7b59eb06b02507d22db673cea4ec13a737c7f673
Bevan Kay <email@bevankay.me>
2024-05-16 17:25:27 +1000

    font/font-m/font-maple: try different url

--- Casks/font/font-m/font-maple.rb
+++ Casks/font/font-m/font-maple.rb
@@ -2,7 +2,7 @@ cask "font-maple" do
   version "6.4"
   sha256 "6b4e892c83f96956fd49e133ea4c3e2ea2b2d3274281a5253c31560366341bc4"
 
-  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-ttf.zip"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-otf.zip"
   name "Maple Mono"
   desc "Nerd Font font with round corners"
   homepage "https://github.com/subframe7536/Maple-font"
